7a248ab0274881df3a40c8c4f060585ba98e60e8
alexcip999 <58331044+alexcip999@users.noreply.github.com>
2024-12-07 14:03:46 +0200

    Server CarSelling-App commit

--- src/main/kotlin/com/example/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/com/example/repository/UserRepositoryImpl.kt
@@ -11,19 +11,19 @@ class UserRepositoryImpl(
 ): UserRepository {
     override suspend fun registerUser(params: CreateUserParams): BaseResponse<Any> {
         return if (isUsernameExist(params.username)) {
-            BaseResponse.ErrorResponse("Username already exists")
+            BaseResponse.ErrorResponse("Username already exists", message = "Error")
         }else{
             if (params.password == params.passwordConfirmation){
                 val user = userService.registerUser(params)
                 if(user != null){
                     val token = JwtConfig.instance.createAccessToken(user.id)
                     user.authToken = token
-                    BaseResponse.SuccessResponse(user)
+                    BaseResponse.SuccessResponse(user, message = "Success")
                 }else{
-                    BaseResponse.ErrorResponse("Invalid username or password")
+                    BaseResponse.ErrorResponse("Invalid username or password", message = "Error")
                 }
             } else{
-                BaseResponse.ErrorResponse("Passwords don't match")
+                BaseResponse.ErrorResponse("Passwords don't match", message = "Error")
             }
 
         }
@@ -32,13 +32,13 @@ class UserRepositoryImpl(
     override suspend fun loginUser(username: String, password: String): BaseResponse<Any> {
         val user = userService.findUserByUsername(username)
         return if (user != null){
-            if (user.password == hash(password)){
-                BaseResponse.SuccessResponse("Success logging")
+            if (user.password == password){
+                BaseResponse.SuccessResponse("Success logging", message = "Success")
             }else{
-                BaseResponse.ErrorResponse("Incorrect password")
+                BaseResponse.ErrorResponse("Incorrect password", message = "Error")
             }
         }else{
-            BaseResponse.ErrorResponse("Invalid username or password")
+            BaseResponse.ErrorResponse("Invalid username or password", message = "Error")
         }
     }
 
--- src/main/kotlin/com/example/utils/BaseResponse.kt
+++ src/main/kotlin/com/example/utils/BaseResponse.kt
@@ -3,16 +3,18 @@ package com.example.utils
 import io.ktor.http.*
 
 sealed class BaseResponse<T>(
-    val statusCode: HttpStatusCode = HttpStatusCode.OK,
+    val statusCode: HttpStatusCode,
 ) {
     data class SuccessResponse<T>(
         val data: T? = null,
-        val message: String? = null
-    ): BaseResponse<T>()
+        val message: String? = null,
+        val statusCodeSucces: HttpStatusCode = HttpStatusCode.OK,
+    ): BaseResponse<T>(statusCodeSucces)
 
     data class ErrorResponse<T>(
         val data: T? = null,
-        val message: String? = null
-    ): BaseResponse<T>()
+        val message: String? = null,
+        val statusCodeError: HttpStatusCode = HttpStatusCode.BadRequest,
+    ): BaseResponse<T>(statusCodeError)
 
 }
